9c8786253a23cf044a26f83501c4d70c7a5275b2
naveenchander30 <naveenmoxx@gmail.com>
2025-06-07 19:56:05 +0530

    Removing need for refreshing spotify tokens

--- auth_flask.py
+++ auth_flask.py
@@ -62,33 +62,7 @@ def get_spotify_tokens():
     with open('spotify_tokens.json', 'r') as f:
         tokens = json.load(f)
     return jsonify(tokens)
-@app.route('/spotify/refresh', methods=['POST'])
-def refresh_spotify_token():
-    refresh_token = request.args.get('refresh_token')
-    if not refresh_token:
-        return jsonify({"error": "No refresh token found"}), 400
-    params = {
-        "grant_type": "refresh_token",
-        "refresh_token": refresh_token
-    }
-    headers={
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Basic " + base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
-    }
-    token_url = "https://accounts.spotify.com/api/token"
-    response = requests.post(token_url, headers=headers, data=params)
-    if response.status_code == 200:
-        data = response.json()
-        access_token = data.get('access_token')
-        if 'refresh_token' in data:
-            refresh_token = data.get('refresh_token')
-        expiry = data.get('expires_in')
-        tokens = {
-            "access_token": access_token,
-            "refresh_token": refresh_token,
-            "expiry": time.time() + expiry
-        }
-        return tokens
+
             
 if __name__ == "__main__":
     app.run(host="0.0.0.0",debug=True, port=int(os.getenv("PORT", 8080)))
--- spotify_valid.py
+++
@@ -1,8 +0,0 @@
-import time,requests,json
-def valid_access_token(tokens):
-    expiry = tokens.get('expiry')
-    if time.time() > expiry:
-        response = requests.post('https://musync-k60r.onrender.com/spotify/refresh',params={'refresh_token': tokens.get('refresh_token')})
-        if response.status_code == 200:
-            tokens = response.json()
-            return tokens
